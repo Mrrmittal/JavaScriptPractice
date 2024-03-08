4675b598d45c65f5d5f67272825509d8175c8077
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-09 02:03:48 +0530

    coding practice

---
+++ CodingPractice/ParkerPen.js
@@ -0,0 +1,10 @@
+// WAP to create an object of ParketPen
+
+const product = {
+    title : "Parker Pen",
+    price : 280,
+    offer : 5,
+    rating : 4
+}
+
+console.log(product);
---
+++ CodingPractice/PrintEvenNumber.js
@@ -0,0 +1,6 @@
+// WAP to print all even number 0 to 100
+for(let i =0; i<=100; i++){
+    if(i%2 === 0){
+        console.log(i);
+    }
+}
