959edf99906166b40ce1821672e36d8c4cacb42c
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-09 15:18:26 +0530

    String

---
+++ CodingPractice/String.js
@@ -0,0 +1,17 @@
+/*
+Que: What is String ? And how to create Strings?
+Sol: JavaScript String is a sequence of characters, typically used to represent text.
+     It is enclosed in single or double quotes and supports various methods for text manipulation.
+
+    Declaration of a String:-
+    1.) By Using Single Quote('')
+    2.) By Using Double Quote("")
+    3.) By Using backtics (``) 
+     */
+
+    let fullName1 = "Jatin Mital 1";
+    let fullName2 = 'Jatin Mittal 2';
+    let fullName3 = `Jatin Mittal 3`;
+    console.log(fullName1);
+    console.log(fullName2);
+    console.log(fullName3);
