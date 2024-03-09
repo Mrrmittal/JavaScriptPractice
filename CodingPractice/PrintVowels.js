bff6ed1359a89badadd0f7e33adc7318735e0df4
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-10 01:27:36 +0530

    CountVowels

--- CodingPractice/PrintVowels.js
+++ CodingPractice/PrintVowels.js
@@ -1,8 +1,21 @@
 // WAP to count vowel in the given string
 
 let fullName = "Jatin Mittal";
-let count = 0;
+
+
+// By using normal funtion 
 function countVowels(fullname){
+    let count = 0;
+    for(const char of fullName){
+        if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' )
+            count++;
+    }
+    console.log(`Total vowel in this fullName is : ${count}`);
+}
+
+// By using arrow function
+const countVowels2 = (fullName) => {
+    let count = 0;
     for(const char of fullName){
         if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' )
             count++;
@@ -10,3 +23,4 @@ function countVowels(fullname){
     console.log(`Total vowel in this fullName is : ${count}`);
 }
 countVowels(fullName);
+countVowels2(fullName);
