b7b14b5afbfa09e8af13a563990b00ae8d820ea9
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-10 15:42:11 +0530

    Square an array

--- CodingPractice/PrintSquare.js
+++ CodingPractice/PrintSquare.js
@@ -1,9 +1,9 @@
 // WAP to print the square of each value by using the forEach Loop.
 
 let digit = [1,2,3,4,5,6,7,8];
-let output = [];
 
-digit.forEach((val) => {
-        output.push(val*val);
+
+let output =  digit.map((val) => {
+        return val * val;
 })
 console.log(output);
