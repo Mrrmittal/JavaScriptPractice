59a3220f977fef07a4b4b11d503acbec71f60895
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-11 13:02:26 +0530

    Array Constructor

---
+++ BasicDetails/ArrayConstructor.js
@@ -0,0 +1,14 @@
+/*
+Que : What is an Array Constructor?
+Sol: The Array() constructor is used to create Array objects and the array constructor can be called
+     with or without a new keyword, both can create a new Array.
+*/
+
+let arr = new Array(1,2,3);
+arr.push("Jatin");
+arr.push("Rohit");
+arr.push("Nitin");
+arr.push("Mohit");
+console.log(arr);
+
+console.log(arr.length); // arr.length to call the length of an array.
