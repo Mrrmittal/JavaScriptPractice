aba42b91a2a0194518f36b52b16363bed9e9f7b7
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-11 13:41:37 +0530

    Print Largest Element

---
+++ CodingPractice/LargestElement.js
@@ -0,0 +1,14 @@
+// WAP to print the largest element of an given array
+
+let array = [45,78,96,25,36,78,15];
+
+
+let largestElement = array.reduce((res, current) =>{
+    if(res > current ){
+        return res;
+    } else{
+        return current;
+    }
+})
+
+console.log(largestElement);
