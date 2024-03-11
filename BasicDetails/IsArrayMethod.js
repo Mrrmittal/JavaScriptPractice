7452a3a7c926da8c1d0d3653abf7baf16ca53751
Jatin Mittal <jatinmittal164@gmail.com>
2024-03-11 13:29:03 +0530

    Array.from() method

---
+++ BasicDetails/IsArrayMethod.js
@@ -0,0 +1,13 @@
+/*
+Que: What is an isArray() method? How to use?
+Sol: The Javascript arr.isArray() method determines whether the value passed to this function is an array
+     or not. This function returns true if the argument passed is an array else it returns false.
+
+     
+*/
+
+let arr = [1,2,3,4,5,6];
+
+// To check arr is Array or not
+let a = Array.isArray(arr);
+console.log(a); // Output = true
